add98217839ab8327b41d05bbb55e02aac0b789f
devzhou <zhouyu@devzhou.cn>
2019-04-28 20:52:15 +0800

    First commit

---
+++ src/main/java/cn/devzhou/timeserver/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package cn.devzhou.timeserver.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "ok";
+    }
+}
---
+++ src/main/java/cn/devzhou/timeserver/TimeserverApplication.java
@@ -0,0 +1,14 @@
+package cn.devzhou.timeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeserverApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TimeserverApplication.class, args);
+    }
+
+}
